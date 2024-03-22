f575969fd854f340cd191ea6ba433d3496bf0a43
ArtthSilva <arthur12pereira1@gmail.com>
2024-03-22 16:55:30 -0300

    first commit

---
+++ lista-exercicios-views2/questao2.sql
@@ -0,0 +1,10 @@
+--Criar uma view que apresente a cidade com o maior número de vendas em 2023, com a quantidade total de vendas e o valor total das vendas.
+CREATE MATERIALIZED VIEW vw_vendas_cidades AS
+	SELECT
+		u.cidade,
+		SUM(v.quantidade) AS total_vendas,
+		SUM(v.valor_compra) AS total_valor
+	FROM usuarios u
+	JOIN vendas v ON u.cod_usuario = v.cod_usuario
+	GROUP BY u.cidade
+	ORDER BY total_vendas DESC;
